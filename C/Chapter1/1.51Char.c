e5df488c333a4ea2c831d19c0cefd2a46d39b262
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 16:59:15 -0500

    Changes with new for loop

--- C/Chapter1/1.51Char.c
+++ C/Chapter1/1.51Char.c
@@ -3,10 +3,13 @@
 int main(){
     int c; //initializes an int since we are dealing with a stream 
 
-    c = getchar(); //reads the characters
-    while(c != EOF){ //for loop tests until it reaches the end of the file
-        putchar(c); //output the char read
-        c= getchar(); //reads the next character in the stream
+    //c = getchar(); //reads the characters
+    //while(c != EOF){ //for loop tests until it reaches the end of the file
+        //putchar(c); //output the char read
+       // c= getchar(); //reads the next character in the stream
+
+    while ((c= getchar()) != EOF){
+        putchar(c);
     }
     return 0;
 }
