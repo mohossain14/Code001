20fb235d6a6c8ba3343ba134ca5d48cba569e52a
Mohammad Hossain <mo.hossain14@outlook.com>
2021-12-25 23:45:41 -0500

    New changes to just the hello world file

--- C/hello world.c
+++ C/hello world.c
@@ -5,5 +5,6 @@ int main(){
     printf("What is your age? ");
     scanf("%d", &age);
     printf("Your age is %d\n",age);
+    printf("Congrats on being around the world at age %d\n",age);
     return 0; 
 }
