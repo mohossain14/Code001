8e32e62bdaf69776bb7fcb77adb882ef4b6ba256
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 00:53:28 -0500

    No real changes

--- C/hello world.c
+++ C/hello world.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //include info about the standard library
 
 main(){ //define a function that receives no arguments
-    printf("Hello world\n"); //calls printf fx that prints the characters
+    printf("\tHello world"); //calls printf fx that prints the words we want
     return 0;
 }
