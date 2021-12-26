1f564898da42720e567ab83d4e52ea744e83875a
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 02:36:59 -0500

    Did reverse order for loop

---
+++ C/Chapter1/1.35ReverseFor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int fahr;
+    printf("Fahrenheit\tCelsius\n");
+
+    for(fahr = 300; fahr >= 0; fahr = fahr -20){
+        printf("%3d\t\t%6.2f\n", fahr, (5.0/9.0)*(fahr - 32));
+    }
+    return 0;
+}
