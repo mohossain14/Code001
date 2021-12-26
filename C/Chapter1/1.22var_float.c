f41765dbff45076c1cca423eb2e858b18203ee84
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 01:28:09 -0500

    New folder

--- C/Chapter1/1.22var_float.c
+++ C/Chapter1/1.22var_float.c
@@ -3,7 +3,7 @@
 //print the Fahrenheit-Celcius table from 0 to 300
 
 int main(){
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, steps;
 
     lower = 0; //lower limit of temp table
@@ -13,7 +13,7 @@ int main(){
     
     printf("Celsius\t\tFahrenheit\n");
     while (fahr <= upper){ //loops through until it is equal to upper limit
-        celsius = 5 * (fahr-32) / 9; //the equation we want
+        celsius = (5.0/9.0)* (fahr-32); //the equation we want in float mode to get more accurate
         printf("%3d\t\t%5d\n", fahr, celsius); //the %3d right justifies to 3 digits likwise %6d is 6 digits by giving it width parameters
         fahr = fahr + steps;
     }
