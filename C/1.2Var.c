a116b93c8078e1fd15ede2d0ee09f6513b0f6edc
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 01:12:02 -0500

    Changed spacing

--- C/1.2Var.c
+++ C/1.2Var.c
@@ -9,8 +9,8 @@ int main(){
     lower = 0; //lower limit of temp table
     upper = 300; //upper limit
     steps = 20; //step size 
-
     fahr = lower; //initialize the want variable
+    
     printf("Celsius\t\tFahrenheit\n");
     while (fahr <= upper){ //loops through until it is equal to upper limit
         celsius = 5 * (fahr-32) / 9; //the equation we want
