ea08a22a6808785a2884db2851aafa06e06090c5
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-25 20:09:23 -0500

    my first edt on alt branch

--- C/hello.c
+++ C/hello.c
@@ -4,6 +4,6 @@
 int main(){
     printf("Welcome to NFS: SpEeD, lets see if this even worls if you have what it takes, we'll see when you come to town\n");
     printf("There is nothing like home in the great unknown\n");
-    printf("I decided to add stuff on github, lets see if it shows up on VS when I take down the goalie\n");
+    printf("If I'm corret I would right now be  in the alternate branch");
     return 0;
 }
