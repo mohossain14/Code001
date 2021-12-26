50b20a39d2ea8bd1602b190933529b4161f02d5f
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-25 20:11:26 -0500

    These are my current changes

--- C/hello.c
+++ C/hello.c
@@ -4,6 +4,6 @@
 int main(){
     printf("Welcome to NFS: SpEeD, lets see if this even worls if you have what it takes, we'll see when you come to town\n");
     printf("There is nothing like home in the great unknown\n");
-    printf("If I'm corret I would right now be  in the alternate branch");
+    printf("GThis is the master branch if I'm correct");
     return 0;
 }
