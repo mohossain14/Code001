146d4ee8c3a030d7021e608b477a71a1cd95f07d
Mohammad A. Hossain <mo.hossain14@outlook.com>
2021-12-26 20:02:35 -0500

    changes

--- C/hello world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> //include info about the standard library
-
-main(){ //define a function that receives no arguments
-    printf("\tHello world"); //calls printf fx that prints the words we want
-    return 0;
-}
--- C/hello.c
+++
@@ -1,9 +0,0 @@
-//lets see if this will even work with
-#include <stdio.h>
-
-int main(){
-    printf("Welcome to NFS: SpEeD, lets see if this even worls if you have what it takes, we'll see when you come to town\n");
-    printf("There is nothing like home in the great unknown\n");
-    printf("GThis is the master branch if I'm correct");
-    return 0;
-}
