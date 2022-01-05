46bd96ce358fafc2b167d95cadcad66341f226f5
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-05 16:30:46 -0500

    Pulled changes

--- owen.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char player[20];
-    printf("Who is the best Minecraft playa?? ");
-    scanf("%s" , player);
-    printf("The king of Joe's Minecraft server is %s\n", player);
-}
