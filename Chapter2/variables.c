64704e22269a5e559137827ef1971bf6cdecdb0c
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-01-07 22:57:53 -0500

    There isnt anything new

--- Chapter2/variables.c
+++ Chapter2/variables.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    int var;
-    printf("There is a new car that I cannot see if there is nothingbetter than what I was originially thinking of.\n Look there is going to be a brand new car that is coming for the new car that is obviously not the best choice for when you don't have the newest flashiest vehicles.");
-    printf("There is nothing better than watching batman!");
-    printf("There is nothing like watching Iron Man the movie from 2007");
+    
 }
