a6a17856c329b51dd513c8060459e0f199163822
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-01-12 14:46:55 -0500

    Changes to exercise

--- Chapter2/dweight.c
+++ Chapter2/dweight.c
@@ -6,18 +6,17 @@ int main(void){
     //can also be written in one initialization
 
     int height = 8, length =12, width = 10;
-    int volume, weight;
+    int volume;
 
     //height = 8; 
     //length = 12;
     //width = 10;
     volume = height * length * width;
-    weight = (volume + 165)/166;
 
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
     //can also written as one exp within print: printf("%d\n", height*length*width);
-    printf("Dimensional weight (pounds): %d\n", weight);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165)/166); //put the weight formula directly into the print statement, negating the need to declare a variable
     
     return 0;
 }
