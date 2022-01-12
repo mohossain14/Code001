193b48d5d6e7c1a44d80f98e6a0da0917127fae5
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-12 00:24:00 -0500

    New ways of writing print statement

--- Chapter2/dweight.c
+++ Chapter2/dweight.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 
 int main(void){
-    int height, length, width, volume, weight;
+    //int height, length, width, volume, weight;
+    //can also be written in one initialization
 
-    height = 8; 
-    length = 12;
-    width = 10;
+    int height = 8, length =12, width = 10;
+    int volume, weight;
+
+    //height = 8; 
+    //length = 12;
+    //width = 10;
     volume = height * length * width;
     weight = (volume + 165)/166;
 
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
+    //can also written as one exp within print: printf("%d\n", height*length*width) 
     printf("Dimensional weight (pounds): %d\n", weight);
     
     return 0;
