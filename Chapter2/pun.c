5e8960a2e0c44df8490a85dc52e89a9b630a8367
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-11 23:40:10 -0500

    New book

---
+++ Chapter2/pun.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //directives are only one line long and are needed since C has no built in read/write functions
+
+int main(void){ //function that contains a group of statements to be executed
+    printf("To C, or not to C: that is the question.\n"); //semi-colon shows end of statement
+    printf("Brevity is the soul of wit.\n --Shakespeare\n");
+    return 0; //we write this to indicate the end of function and give the expected value of our function since it is int main()
+}
