bd87e352309419c6f0783f76b593389d8b05f273
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-21 09:52:17 -0500

    In progress...

--- Chapter5/proj8.c
+++ Chapter5/proj8.c
@@ -10,5 +10,16 @@ int main(void){
 
     totalTime = (hr * 60) + min;
 
-    
+    if  (totalTime < 480){
+        printf("Closest departure time is 8:00 a.m., arriving at 10:16 a.m.");
+    }
+    else if (totalTime < 583){
+        printf("Closest departure time is 9:43 a.m., arriving at 11:52 a.m.");
+    }
+    else if (totalTime < 679){
+        printf("Closest departure time is 11:19 a.m., arriving at 1:31 p.m.");
+    }
+    else if (totalTime < 767){
+        printf("Closest departure time is 12:47 p.m., arriving at 3:00 p.m.");
+    }
 }
