c64bdb347b517643bfe0af72e4ea3dd9d6459c30
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-20 22:14:17 -0500

    still need to complete

---
+++ Chapter5/proj8.c
@@ -0,0 +1,14 @@
+//gives the closest flight time to given time range
+
+#include <stdio.h>
+
+int main(void){
+    int hr, min, totalTime;
+
+    printf("Enter a 24-hour time: ");
+    scanf("%d:%d", &hr, &min);
+
+    totalTime = (hr * 60) + min;
+
+    
+}
