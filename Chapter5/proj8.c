55a2f51a0658afd48da14fd50b246d2526d049ae
Mohammad Hossain <mo.hossain14@outlook.com>
2022-01-21 11:48:48 -0500

    still wrong...

--- Chapter5/proj8.c
+++ Chapter5/proj8.c
@@ -22,4 +22,17 @@ int main(void){
     else if (totalTime < 767){
         printf("Closest departure time is 12:47 p.m., arriving at 3:00 p.m.");
     }
+    else if (totalTime < 840){
+        printf("Closest departure time is 2:00 p.m., arriving at 4:08 p.m.");
+    }
+    else if (totalTime < 945){
+        printf("Closest departure time is 3:45 p.m., arriving at 5:55 p.m.");
+    }
+    else if (totalTime < 1140){
+        printf("Closest departure time is 7:00 p.m., arriving at 9:20 p.m.");
+    }
+    else if (totalTime < 1305){
+        printf("Closest departure time is 9:45 p.m., arriving at 11:58 p.m.");
+    }
+    return 0;
 }
