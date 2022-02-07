65c41176febc7f6f8574330925902fe014458bd3
Mohammad Hossain <mo.hossain14@outlook.com>
2022-02-07 10:06:34 -0500

    working on it..

---
+++ Chapter6/proj8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int day, start, i;
+
+    printf("Enter number of days in month: ");
+    scanf("%d", &day);
+    printf("Enter starting day of the week: ");
+    scanf("%d", &start);
+
+    for(i = 1, i <= day; i++){
+        
+    }
+
+}
