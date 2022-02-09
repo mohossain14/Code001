0130d4214102fc629225cb60b1ff68fd2c1a9f95
Mohammad Hossain <mo.hossain14@outlook.com>
2022-02-09 09:16:28 -0500

    calender proj

--- Chapter6/proj8.c
+++ Chapter6/proj8.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
 int main(void){
-    int day, start, j, k = 1;
+    int day, start,i , j, r = 0;
 
     printf("Enter number of days in month: ");
     scanf("%d", &day);
     printf("Enter starting day of the week: ");
     scanf("%d", &start);
 
-    for(j = 1; j <= day; j++){
-        k++;
-        if (j % 7 == 0){
-            printf("\n");
-            printf("%d ", j);
-            k = 0;
-        }
-        else {
-            printf("%d ", j);
-        }
+    for(j = 1; j < start; j++){ //loops through
+        printf("  ");
     }
+    r = start - 1; //r is our tracker for the number of days within the week
 
+    for(i = 1; i <= day; i++){ //main loop that iterates the days
+        if ((r % 7) == 0){ // is the tracker has mod 0 then its the end of the week
+            printf("\n");
+            printf("%2d ", i); //prints the day for the following week 
+            r++; //increments
+        }
+        else{
+            printf("%2d ", i); //prints out the days
+            r++;
+        }
+
+    }
     return 0;
 }
