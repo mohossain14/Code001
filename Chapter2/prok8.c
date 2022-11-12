9c87c41fe84d5e3defd4b163a6f7826ea55d8c98
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-11-12 03:35:50 -0500

    11/12

--- Chapter2/prok8.c
+++ Chapter2/prok8.c
@@ -1,27 +1,26 @@
-//program that calculates the monthly loan balance
+//write a program that calculates remaining balance 
 
 #include <stdio.h>
+#define MONTH 12
 
 int main(void){
-    float loan, interestRate, monthlyPayment;
-    float monthRate, tot1, tot2, tot3;
+    float amount, rate, payment, rate2;
 
-    printf("Enter the amount of the loan: "); //scan in the parameters we need
-    scanf("%f", &loan);
+    printf("Enter loan amount: ");
+    scanf("%f", &amount);
     printf("Enter the interest rate: ");
-    scanf("%f", &interestRate);
-    printf("Enter monthly payments: ");
-    scanf("%f", &monthlyPayment);
+    scanf("%f", &rate);
+    printf("Enter monthly payment: ");
+    scanf("%f", &payment);
 
-    monthRate = (interestRate / 100.0f) / 12.0f; //calculate the monthly interest rate by converting to percetnage and then dividing my total months in a year
+    rate2 = (rate / 100) / MONTH;
 
-    tot1 = (loan - monthlyPayment) + ((loan - monthlyPayment) * monthRate); //we find the balance by first subtracting the monthly payments but then findign the interest and adding it the the total balance left
-    tot2 = (tot1 - monthlyPayment) + ((tot1 - monthlyPayment) * monthRate);
-    tot3 = (tot2 - monthlyPayment) + ((tot2 - monthlyPayment) * monthRate);
-
-    printf("Balance remaining after first payment: %.2f\n", tot1); //print out the results
-    printf("Balance remaining after second payment: %.2f\n", tot2);
-    printf("Balance remaining after third payment: %.2f\n", tot3);
+    amount = ((amount - payment) * rate2) + (amount - payment);
+    printf("Balance remaing after first payment: %.2f\n", amount);
+    amount = ((amount - payment) * rate2) + (amount - payment);
+    printf("Balance remaing after second payment: %.2f\n", amount);
+    amount = ((amount - payment) * rate2) + (amount - payment);
+    printf("Balance remaing after third payment: %.2f\n", amount);
 
     return 0;
 }
