c8018fa0229e6fede6df0e560b235fbd2acd8917
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-11-12 03:05:54 -0500

    11/12 changes

--- Chapter2/proj5.c
+++ Chapter2/proj5.c
@@ -1,28 +1,24 @@
-//write a program that takes amounts and divides it to find the total needed to pay
+//write a program that takes the total and finds the total number of certain bills that are required 
 
 #include <stdio.h>
 
 int main(void){
-    int total, twenty, ten, five, one; //need to declare the variables in use
+    int amount, twenty, ten, five, one;
 
-    printf("Enter the dollar amount you've spent: ");
-    scanf("%d", &total);
+    printf("Enter a dollar amount: ");
+    scanf("%d", &amount);
+    twenty = amount / 20;
+    amount = amount - (twenty * 20);
+    ten = amount / 10;
+    amount = amount - (ten * 10);
+    five = amount / 5;
+    amount = amount - (five * 5);
+    one = amount;
 
-    twenty = total / 20; //to find number of bills just divide by the amount
-    total = total - (20 * twenty); //then modify the total amount for the next amount since we dont need to consider 20 bills anymore
-
-    ten = total / 10; //repeat the process
-    total = total - (10 * ten);
-
-    five = total / 5;
-    total = total - (5 * five);
-
-    one = total / 1;
-    
-    printf("$20 bills: %d\n", twenty); //print out all the expected results
+    printf("$20 bills: %d\n", twenty);
     printf("$10 bills: %d\n", ten);
-    printf("$5 bills: %d\n", five);
-    printf("$1 bills: %d\n", one);
+    printf(" $5 bills: %d\n", five);
+    printf(" $1 bills: %d\n", one);
 
-
+    return 0;
 }
