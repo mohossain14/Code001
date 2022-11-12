3079419c3063adfc61cb8013b7828c82554513d4
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-11-12 02:37:15 -0500

    Practice 11/12

--- Chapter2/dweight.c
+++ Chapter2/dweight.c
@@ -1,22 +1,18 @@
-//computes the dimensional 
+//computes the dimensions of a box 
 #include <stdio.h>
 
 int main(void){
-    //int height, length, width, volume, weight;
-    //can also be written in one initialization
+    int height, length, width, volume, weight;
 
-    int height = 8, length =12, width = 10;
-    int volume;
-
-    //height = 8; 
-    //length = 12;
-    //width = 10;
+    height = 8;
+    length = 12;
+    width = 10;
     volume = height * length * width;
+    weight = (volume + 165) / 166;
 
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
-    //can also written as one exp within print: printf("%d\n", height*length*width);
-    printf("Dimensional weight (pounds): %d\n", (volume + 165)/166); //put the weight formula directly into the print statement, negating the need to declare a variable
-    
+    printf("Dimensional weight (pounds): %d\n", weight);
+
     return 0;
 }
