11abf00aa145942506a86ea7d787309fff9c8da1
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-11-29 09:00:29 -0500

    478 11/29

--- Chapter2/478.c
+++ Chapter2/478.c
@@ -32,8 +32,116 @@ if state = clear
 
 
 //drawing letter Y
-if(hcount > 349 and hcount < 649 and vcount > 100 and vcount < 321) then 
+if(vcount > 335 and vcount < 360 and hcount > 308 and hcount < 321) then 
         red_data <= (others => '0');
         green_data <= (others => '0');
         blue_data <= (others => '0');
-elsif()
+elsif(vcount > 295 and vcount < 335 and hcount > 321 and hcount < 334) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 335 and vcount < 360 and hcount > 334 and hcount < 347) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 425 and hcount > 362 and hcount < 372) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+//letter O
+elsif(vcount > 295 and vcount < 425 and hcount > 362 and hcount < 372) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 305 and hcount > 362 and hcount < 404) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 415 and vcount < 425 and hcount > 362 and hcount < 404) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 425 and hcount > 394 and hcount < 404) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+
+//letter U
+elsif(vcount > 295 and vcount < 425 and hcount > 419 and hcount < 432) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 308 and hcount > 432 and hcount < 458) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 425 and hcount > 445 and hcount < 458) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+
+//letter L
+elsif(vcount > 295 and vcount < 425 and hcount > 493 and hcount < 506) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 310 and hcount > 493 and hcount < 536) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+
+//letter O
+elsif(vcount > 295 and vcount < 425 and hcount > 549 and hcount < 562) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 308 and hcount > 549 and hcount < 591) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 415 and vcount < 425 and hcount > 549 and hcount < 591) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 425 and hcount > 581 and hcount < 591) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+//letter S
+elsif(vcount > 295 and vcount < 308 and hcount > 604 and hcount < 646) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 321 and hcount > 633 and hcount < 646) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 308 and vcount < 321 and hcount > 604 and hcount < 646) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 386 and vcount < 425 and hcount > 604 and hcount < 617) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 412 and vcount < 425 and hcount > 604 and hcount < 646) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+//letter E
+elsif(vcount > 295 and vcount < 308 and hcount > 661 and hcount < 691) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 295 and vcount < 425 and hcount > 661 and hcount < 674) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 321 and vcount < 334 and hcount > 661 and hcount < 691) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
+elsif(vcount > 321 and vcount < 334 and hcount > 661 and hcount < 691) then 
+        red_data <= (others => '0');
+        green_data <= (others => '0');
+        blue_data <= (others => '0');
