6d9b4c5a52afbad4e5b977b24780a77972a335a3
Mohammad Hossain <mo.hossain14@outlook.com>
2022-12-18 23:00:41 -0500

    starting over

--- Chapter8/reverse.c
+++ Chapter8/reverse.c
@@ -1,21 +1,22 @@
-//reverses numbers put into arrays
+//reverses a series of numbers 
 
 #include <stdio.h>
-#define N 4
+
+#define N 5
 
 int main(void){
     int a[N], i;
 
-    printf("Enter %d numbers: \n", N);
-    for (i = 0; i < N; i++){
-        printf("Number %d: ", i + 1);
+    printf("Enter %d numbers: ",N);
+    for (i=0; i<N; i++){
         scanf("%d", &a[i]);
     }
 
-    printf("In reverse order: ");
-    for (i = N - 1; i >= 0; i--){
-        printf("%d ", a[i]);
+    printf("In reverse oder: ");
+    for (i= N-1; i >= 0; i--){
+        printf(" %d ", a[i]);
     }
     printf("\n");
+
     return 0;
 }
